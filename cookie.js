d1625395cdddd7b604dc75617299ca9dad673f6c
Vitor Miguel Santos Redondo <140042669+VitinhoUlti@users.noreply.github.com>
2023-08-28 19:02:08 -0300

    versão 0.2
    colocando mais conteudo, e corrigindo bugs

--- cookie.js
+++ cookie.js
@@ -1,9 +1,33 @@
+let real = window.document.getElementById("real")
+let imagem = window.document.getElementById("imagem")
 let pontos = 0
+let n = pontos+1
+let b = 0
+
+function dou(){
+    if(pontos<10){
+        window.alert("tu é pobre kkkk, precisa de 10 conto pra comprar")
+    }
+    else{
+    n = pontos+1
+    pontos = n
+    b = b+1
+    pontos = pontos-11
+    real.textContent = pontos
+    }
+}
+
 function brasil(){
     let real = window.document.getElementById("real")
-    let n = pontos+1
-    pontos = n
-    real.textContent = pontos
+    if(b<=0){
+       let n = pontos+1
+       pontos = n 
+    }
+    else if(b>=1){
+        n = pontos+2
+        pontos = n
+    }
+    real.textContent = pontos 
     if(pontos>=100){
     window.alert("kkkkkk troxão, clicou 100 vezes sem nem ter oque comprar")
 }
